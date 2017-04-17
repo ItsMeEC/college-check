f74cf707c547dbf70d61baac75027a42e4ad5b60
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-04-17 18:25:09 -0400

    models

---
+++ models/collegeModel.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+    
+    
+var collegeModel = new Schema({
+	college_name:{type:String},
+	state:{type:String},
+	value:{type:String},
+	website:{type:String},
+	twitter:{type:String},
+	facebook:{type:String},
+	instagram:{type:String}
+
+    
+	
+});
+
+module.exports = mongoose.model('College', collegeModel, 'college');
