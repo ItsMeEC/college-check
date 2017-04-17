1a5be607ee8af7cd2b94675b77cf7f4929e66940
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-04-17 18:22:43 -0400

    api

---
+++ api.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+var mongoose =require('mongoose');
+var db = mongoose.connect('mongodb://localhost/college');
+var Col = require('./models/collegeModel');
+var bodyParser = require('body-parser');
+const passport = require('passport');
+
+var app = express();
+
+const users = require('./routes/users');
+
+var port = process.env.PORT||3000; 
+var commanRouter = express.Router();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+commanRouter.route('/getColleges')
+    .get(function(req,res){
+		Col.find(function(err,data){
+			if(err)
+			   res.status(500).send(err);
+			else
+				res.json(data);
+		})
+});
+
+app.use('/api', commanRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+	res.send('welcome to my api');
+});
+
+app.get('/image', function(req,res){
+              res.end(img, 'binary');
+});
+
+app.listen(port, function(){
+	console.log("running");
+});
